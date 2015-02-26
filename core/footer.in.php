34b8c5501bd403715420806f3babb256830482bc
Sergio Ortiz Crespo <sergiooc@gmail.com>
2015-02-26 18:12:33 +0100

    first commit

---
+++ core/footer.in.php
@@ -0,0 +1,7 @@
+<footer>
+<p><span>Made with</span> Love in Madrid</p>			
+</footer>
+<script  type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
+<script  type="text/javascript" src="js/bh.js"></script>
+</body>
+</html>                             
---
+++ index.php
@@ -0,0 +1,59 @@
+<?php require("core/doctype.in.php"); ?>
+<?php require("core/header.in.php"); ?>
+
+
+<header>
+<h1><span>i.24K</span></h1>
+<nav>
+	<ul>
+		<li><a href="#">Services</a></li>
+		<li><a href="#tech">Technologies</a></li>
+		<li><a href="#about">About</a></li>
+		<li><a href="#contact">Contact</a></li>
+	</ul>
+</nav>
+</header>
+
+<section id="one">
+	<article>
+		<img src="img/textOne.png" alt="Artisans of the web  since 1999" title="Artisans of the web  since 1999"/>
+	</article>
+</section>
+
+<section id="tech">
+<article>
+<ul>
+	<li class="web">
+		<span class="icon"></span>
+		<span>Web</span>
+	</li>
+	<li class="mobile">
+		<span class="icon"></span>
+		<span>Mobile</span>
+	</li>
+	<li class="ux">
+		<span class="icon"></span>
+		<span>UX</span>
+	</li>
+	<li class="iot">
+		<span class="icon"></span>
+		<span>IOT</span>
+	</li>
+</ul>
+	</article>
+</section>
+
+<section id="contact">
+<article>
+	<form action="">
+	<div class="description"><p>A un Angleso it va semblar un simplificat Angles, quam un skeptic Cambridge amico dit me que  Occidental es.Li Europan lingues es membres del sam familie</p></div>
+	<div class="form">
+		<input type="text" value="Contact me" />
+		<button><span>Enviar</span></button>
+	</div>
+	</form>
+</article>	
+</section>
+
+			
+<?php require("core/footer.in.php"); ?>
