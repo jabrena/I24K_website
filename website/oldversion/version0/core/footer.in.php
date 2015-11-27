cf38af6e3ba72f23ad525071f841acd60ddbbcc4
Sergio Ortiz Crespo <sergiooc@gmail.com>
2015-11-27 13:52:17 +0100

    Update directories site

--- website/oldversion/version0/core/footer.in.php
+++
@@ -1,7 +0,0 @@
-<footer>
-<p><span>Made with</span> Love in Madrid</p>			
-</footer>
-<script  type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
-<script  type="text/javascript" src="js/bh.js"></script>
-</body>
-</html>                             
